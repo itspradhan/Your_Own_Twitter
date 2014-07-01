6526fea8f0037082dabbfc64ab2d306287a72311
George <itspradhan@gmail.com>
2014-07-01 04:13:37 +0000

    Microposts Added

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
+  get 'microposts/create'
+
+  get 'microposts/destroy'
+
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
+  resources :microposts, only: [:create, :destroy]
   root 'static_pages#home'
   
   get 'static_pages/home'
